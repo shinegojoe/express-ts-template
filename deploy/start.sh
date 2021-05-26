7592505cfab8e8a74ece0248789c49d4233cb35b
taka <taka@bxb.tw>
2021-05-26 19:11:02 +0800

    fix deploy

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,2 +1,2 @@
 tar -zxvf vocabulary-trainer-server.tar.gz
-NODE_ENV=production pm2 start npm -- run start:dev --name vocabulary-trainer-server
+NODE_ENV=production pm2 start node dist/index.js --name vocabulary-trainer-server
